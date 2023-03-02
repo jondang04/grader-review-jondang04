8e44d732b5b4de075b59a4101943f770af86c588
jondang04 <122576136+jondang04@users.noreply.github.com>
2023-03-02 15:10:20 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,5 +1,24 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
+em -r lab3
 git clone $1 student-submission
 echo 'Finished cloning'
+
+cd student-submission
+if[[ -f ListExamples.java]]
+    then 
+        echo 'ListExamples.java found'
+    else 
+        echo 'ListExamples.java needed'
+        exit
+fi
+cp ../TestListExamples.java ./
+cp -r ../lib ./
+javac -cp $CPATH *.java
+if[[ $? -ne 0]] 
+    then    
+        echo 'Compile Failed'
+        exit
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
